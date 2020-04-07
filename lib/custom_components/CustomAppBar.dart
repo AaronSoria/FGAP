d24f873827183e4de0d1f7942f007e4347a30593
Aaron Soria <soria_aaron@outlook.com>
2020-04-07 14:36:39 -0300

    appbar listo

---
+++ lib/custom_components/CustomAppBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends AppBar{
+  CustomAppBar({Key key, Widget title, bool isBack, Function() onLeadingPressed }): 
+  super(key:key, title: title, 
+  leading: new IconButton(
+    icon: (!isBack) ? new Icon(Icons.menu) : new Icon(Icons.arrow_back),
+    onPressed: () => onLeadingPressed(),
+  ), 
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.vertical(
+      bottom: Radius.circular(30)
+    )
+  ));
+}
